6dc0dfe0f89519dffa2c35b355f4cf6f105fd97e
VeenaArv <veenalarvind@gmail.com>
2020-11-28 17:49:13 -0500

    fix bugs, working http requests

--- test_rqlite.go
+++ test_rqlite.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	// "io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -16,52 +17,80 @@ type RQLiteTest interface {
 // Table is an RQLite table with name `name` and can be accessed using HTTP rquests at `endpoint`.
 // RQLite nodes must be setup before accessing Table.
 type Table struct {
-	endpoint string
-	name     string
+	port int
+	name string
 }
 
 // NewTable creates and returns a pointer to an instance of an Table.
 func NewTable(port int, name string) *Table {
-	endpoint := fmt.Sprintf("localhost:%d/db/query?level=strong", port)
-	return &Table{endpoint, name}
+	return &Table{port, name}
 }
 
 func (table Table) Read() {
 	// Always enable strong consistency which garuntees linearizability. See
 	// https://github.com/rqlite/rqlite/blob/master/DOC/CONSISTENCY.md for
 	//different consistency garuntees.
+	// TODO(VeenaArv): add flag to switch between different consistency levels.
 	query := fmt.Sprintf("SELECT * FROM %s", table.name)
-	runQuery(table, query, "GET")
+	getQuery(query, table.port)
 }
 
 // TODO(veenaarv): update this function to insert values of any type and
 // support multiple columns.
 // Table must be created using `CreateTable`, before writing to it.
 func (table Table) Write(value int) {
-	query := fmt.Sprintf("INSERT INTO %s(value) VALUES(%d)", table.name, value)
-	runQuery(table, query, "POST")
+	query := fmt.Sprintf("[\"INSERT INTO %s(value) VALUES(%d)\"]", table.name, value)
+	postQuery(query, table.port)
 }
 
 func (table Table) CreateTable() {
-	query := fmt.Sprintf("CREATE TABLE %s (value INTEGER)", table.name)
-	runQuery(table, query, "POST")
+	query := fmt.Sprintf("[\"CREATE TABLE %s (value INTEGER)\"]", table.name)
+	postQuery(query, table.port)
 }
 
-func runQuery(table Table, query string, httpMethod string) *http.Request {
-	req, _ := http.NewRequest(httpMethod, table.endpoint, nil)
-	q := req.URL.Query()          // Get a copy of the query values.
+func postQuery(query string, port int) *http.Response {
+	endpoint := fmt.Sprintf("http://localhost:%d/db/execute?pretty&timings", port)
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer([]byte(query)))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp.Status)
+	return resp
+}
+
+func getQuery(query string, port int) *http.Response {
+	endpoint := fmt.Sprintf("http://localhost:%d/db/query?pretty&timings", port)
+	req, err := http.NewRequest("GET", endpoint, nil)
+	q := req.URL.Query() // Get a copy of the query values.
+
+	q.Add("level", "strong")      // read consisteny always strong.
 	q.Add("q", query)             // Add a new value to the set.
 	req.URL.RawQuery = q.Encode() // Encode and assign back to the original query.
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	fmt.Printf("URL      %+v\n", req.URL)
-	fmt.Printf("RawQuery %+v\n", req.URL.RawQuery)
-	fmt.Printf("Query    %+v\n", req.URL.Query())
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	return req
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp.Status)
+	return resp
 }
 func main() {
-	fmt.Println("Hello, world.")
+	t := NewTable(4001, "test2")
+	t.CreateTable()
+	// t.Write(1)
+	// t.Write(2)
+	t.Read()
 
 }
